38cac10108dbb382a7f3d059e0e34cb4a1e37a5a
JonQosa <99906974+JonQosa@users.noreply.github.com>
2024-07-09 16:10:27 +0200

    commit db

--- database/migrations/2024_07_01_121639_create_image_user_table.php
+++ database/migrations/2024_07_01_121639_create_image_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('image_user', function (Blueprint $table) {
+        Schema::create('user_image', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('image')->nullable();
