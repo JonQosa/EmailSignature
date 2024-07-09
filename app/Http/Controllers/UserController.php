6ec1fb1675437eecdc75b166d3d96a74e4d9b1e2
JonQosa <99906974+JonQosa@users.noreply.github.com>
2024-07-09 15:22:27 +0200

    new update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
                 'company_logo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
                 'company_logo1' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
                 'company_logo2' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-                'company_logo3' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+                'gif' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
             ]);
 
             $user = User::create($validatedDataUser);
